a242e18358c473f0d138674e8ea4fdb14358d4bc
slowback1 <dwobeck@gmail.com>
2019-09-30 15:11:57 -0500

    fixed typo

--- firstTimeSetup.php
+++ firstTimeSetup.php
@@ -7,7 +7,7 @@
             foreach($this->queries as $query) {
                 $this->db->query($query);
             }
-            echo "yeet!";
+
         }
         private $queries = array(
             "CREATE TABLE IF NOT EXISTS `user_login` ( `id` INT(10) NOT NULL AUTO_INCREMENT , `username` VARCHAR(2500) NOT NULL , `password` VARCHAR(2500) NOT NULL , `email` VARCHAR(2500) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB"
